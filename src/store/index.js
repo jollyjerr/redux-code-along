91d1d69519e7e5da533e84e6f6bc8190ebb020ae
jollyjerr <jollyjerr@gmail.com>
2019-10-14 13:46:41 -0600

    add redux devtools middleware

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,4 @@
 import {createStore} from 'redux'
 import {counter} from '../reducers/counter'
 
-export default createStore(counter)
+export default createStore(counter, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
